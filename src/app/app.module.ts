af26728eb63edcaada17942358849a0d88b89fb9
Camilo Guio <camiloguio@gmail.com>
2019-11-12 17:31:24 -0500

    Vinculación con el Json de salida - Inclución Ref

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { TemplateComponent } from './components/template/template.component';
 import { DataComponent } from './components/data/data.component';
+import { TerminosycondicionesComponent } from './components/terminosycondiciones/terminosycondiciones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateComponent,
-    DataComponent
+    DataComponent,
+    TerminosycondicionesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -10,10 +10,36 @@ export class TemplateComponent {
 
   constructor() { }
 
+  contacto:Object = {
+    DatosBasicos: {
+      TipoDocumento: 1,  
+      NumeroDocumento: null,  
+      Nombre1: null,  
+      Celular: null,  
+      CorreoPersonal: null  
+    },
+  
+    DatosFinancieros: {  
+      ActividadEconomica: 1,  
+      ActividadIndependiente: 3,  
+      IngresoMensual: 0  
+    },
+  
+    OtrosDatos: {  
+      AutorizaConsultaCentrales: false,  
+      AutorizaMareigua: false,  
+      ValorFinanciar: 0  
+    }
+  }
+
+  /* [(checked)]="contacto.OtrosDatos.AutorizaMareigua" */
+
   guardar(form:NgForm){
     console.log("Formulario Posteado");
     console.log("Forma ", form);   
-    console.log("Forma.value ", form.value);   
+    console.log("Forma.value ", form.value); 
+    console.log("Contacto", this.contacto);
+      
     
   }
 
