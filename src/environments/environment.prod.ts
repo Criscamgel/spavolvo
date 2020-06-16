4ea2425f62c23c6a2416125fd664864330e682af
Camilo Guio <camiloguio@gmail.com>
2020-06-16 14:16:55 -0500

    Cambio Credenciales y Link de tst

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -32,7 +32,7 @@ export const environment = {
   urlVp: "https://api.premiercredit.co:11444/PremierServices_API_EXT/api/viabilizacion/getviabilizacion",
 
   /* Credentials */
-  username: "Mazda",
-  password: "m4zd4",
+  username: "TuCarro",
+  password: "tuC@rr02019",
   respuesta: {}
 };
