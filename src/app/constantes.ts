612d83bf0bd41d3052a2090453a2ca13e3f2cbc1
Camilo Guio <camiloguio@gmail.com>
2020-06-30 17:58:00 -0500

    Cambio de look an feel Volvo

--- src/app/constantes.ts
+++ src/app/constantes.ts
@@ -1,4 +1,4 @@
 export const constantes = {
     logoSantander: './assets/consumerFinance.png',
-    linkWebAliado: 'https://www.ramlatam.com/co/'
+    linkWebAliado: 'https://volvocarcolombia.com/'
 }
